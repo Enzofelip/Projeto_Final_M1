9bfe11e48856ea1ce2d5b3668155a6a324b4327a
Jessica <jb229507@gmail.com>
2023-08-08 18:02:12 -0300

    correcao_no_caminho

--- Jessy_Quiz/index.js
+++ Jessy_Quiz/index.js
@@ -6,14 +6,14 @@ function verificar() {
     do {
         respostaUsuario = prompt("Digite a letra da resposta").toUpperCase();
         if (!respostas.includes(respostaUsuario)) {
-            alert("Digite uma letra válida.");
+            alert("Digite uma alternativa correta.");
         }
     } while (!respostas.includes(respostaUsuario));
 
     if (respostaUsuario === respostas[0]) {
         alert("Você ganhou!");
         setTimeout(function() {
-            window.location = "/telas/round3.html";
+            window.location = "../tela ganhou/index.html";
         }, 2000);
     } else {
         alert("Você perdeu!");
