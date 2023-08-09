a49c33464f66c23ef45a25587626d43dbfc0f9f3
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2023-08-09 17:10:04 -0300

    atualizando-js

--- Larrissa_Quiz/Quiz/index.js
+++ Larrissa_Quiz/Quiz/index.js
@@ -1,15 +1,40 @@
 
-var responder = document.getElementsByClassName("btnresposta");
+//var responder = document.getElementsByClassName("btnresposta");
+
+//function btnresposta(){
+//var responder = prompt("Javascript é uma linguagem de...");
+            //if (responder == null || responder == "") {
+           // alert("Insira a resposta");
+        //}
+            //else (responder == "programação"); {
+           //alert ("resposta certa");
+            //setTimeout(function() {
+                //window.location = "/tela ganhou/index.html";
+            //}, 2000);
+                    //}
+                //}
+
+var resposta = "programação";
 
 function btnresposta(){
-var responder = prompt("Javascript é uma linguagem de...");
-            if (responder == null || responder == "") {
-            alert("Insira a resposta");
-        }
-            else (responder == "programação"); {
-            alert ("resposta certa");
-            setTimeout(function() {
-                window.location = "/tela ganhou/index.html";
-            }, 2000);
-                    }
-                }
+do {
+    responder = prompt("Javascript é uma linguagem de...").toUpperCase();
+    if (!resposta.includes(resposta)) {
+        alert("Digite uma alternativa correta.");
+    }
+} while {
+    !resposta.includes(resposta)};
+
+if (resposta === resposta[0]) {
+    alert("Você ganhou!");
+    setTimeout(function() {
+        window.location = "../tela ganhou/tela ganhou.gif";
+    }, 2000);
+} else {
+    alert("Você perdeu!");
+    setTimeout(function() {
+        window.location = "../telas/gameOver.html";
+    }, 2000);
+}
+}
+                
