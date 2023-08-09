99b74de2e12ba04f7a3401bc5bd2e16496a4f973
Jessica <jb229507@gmail.com>
2023-08-09 20:19:11 -0300

    correcaoJavaScript

--- Larrissa_Quiz/Quiz/index.js
+++ Larrissa_Quiz/Quiz/index.js
@@ -1,40 +1,24 @@
-
-//var responder = document.getElementsByClassName("btnresposta");
-
-//function btnresposta(){
-//var responder = prompt("Javascript é uma linguagem de...");
-            //if (responder == null || responder == "") {
-           // alert("Insira a resposta");
-        //}
-            //else (responder == "programação"); {
-           //alert ("resposta certa");
-            //setTimeout(function() {
-                //window.location = "/tela ganhou/index.html";
-            //}, 2000);
-                    //}
-                //}
-
 var resposta = "programação";
 
-function btnresposta(){
-do {
-    responder = prompt("Javascript é uma linguagem de...").toUpperCase();
-    if (!resposta.includes(resposta)) {
-        alert("Digite uma alternativa correta.");
-    }
-} while {
-    !resposta.includes(resposta)};
+function btnresposta()
 
-if (resposta === resposta[0]) {
-    alert("Você ganhou!");
-    setTimeout(function() {
-        window.location = "../tela ganhou/tela ganhou.gif";
-    }, 2000);
-} else {
-    alert("Você perdeu!");
-    setTimeout(function() {
-        window.location = "../telas/gameOver.html";
-    }, 2000);
-}
-}
+         {
+            var pergunta = prompt("Digite a resposta");  
+     // Converter a resposta para maiúsculas para evitar problemas com letras minúsculas ou maiúsculas.
+
+                if (pergunta === resposta) {
+                    alert("Você ganhou!");
+                    setTimeout(function() {
+                        window.location = "/telas/round3.html";
+                      }, 2000);
+                } else {
+                    alert("Você perdeu!");
+                    setTimeout(function() {
+                        window.location = "../telas/gameOver.html";
+                      }, 2000);
+
+                }
+
+            }
+         
                 
